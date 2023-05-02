f3d64c85dd38d31f3d7acf1a2c9deb44fa87fc32
jay-p-b-7span <jay.p.b@7span.com>
2023-05-02 11:34:11 +0530

    Updated config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@
 import { defineConfig } from "vite";
 import { createVuePlugin as vue } from "vite-plugin-vue2";
 import { fileURLToPath, URL } from "node:url";
-
+import * as path from "path";
 export default defineConfig({
   //https://vitejs.dev/config/
   plugins: [vue()],
@@ -12,5 +12,23 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
     extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
+  },
+
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "./src/plugin.js"),
+      fileName: "vue-list",
+      name: "VueList",
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled into your library
+      external: ["vue"],
+      output: {
+        // Provide global variables to use in the UMD build for externalized deps
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
   },
 });
